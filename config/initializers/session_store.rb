c70def49e0df4d1a180712a25a255bbef3b28539
Paweł Placzyński <placek@ragnarson.com>
2012-09-10 14:00:16 +0200

    rspec installed; some config files cleanup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
 Cemetery::Application.config.session_store :cookie_store, key: '_cemetery_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Cemetery::Application.config.session_store :active_record_store
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  I18n.default_locale = :pl
+  I18n.locale = :pl
+  config.mock_with :rspec
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
+end
