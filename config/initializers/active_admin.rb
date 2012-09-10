f57be77d98127e8513383b6b669e4304796e4596
Paweł Placzyński <placek@ragnarson.com>
2012-09-10 14:42:13 +0200

    basic administration panel

---
+++ config/initializers/active_admin.rb
@@ -0,0 +1,8 @@
+ActiveAdmin.setup do |config|
+  config.site_title = "Cmentarz 2.0"
+  config.authentication_method = :authenticate_admin_user!
+  config.current_user_method = :current_admin_user
+  config.logout_link_path = :destroy_admin_user_session_path
+  config.logout_link_method = :post
+  config.batch_actions = true
+end
