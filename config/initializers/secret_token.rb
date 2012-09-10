624a6194fff5cfb735408e59726cebea0f8c97c1
Paweł Placzyński <placek@ragnarson.com>
2012-09-10 12:53:00 +0200

    new fresh rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cemetery::Application.config.secret_token = '6a17539614983ecc0bbf216955d196a5466d14dfd117ae60cf068227a2a74edd36b6a1506769910697af069ac480034a6258666f786b3cd57a584505a592bd4d'
