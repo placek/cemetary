9f77d70895077945bbeb784ac1430a2a33a1863b
Paweł Placzyński <placek@ragnarson.com>
2013-03-04 22:37:01 +0100

    social media

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+  def publish_on_facebook post
+    url = "http://www.facebook.com/sharer.php?u=#{post_url(post)}"
+    link_to(image_tag(image_path("social_media/facebook.png")), url, target: "_blank")
+  end
+  def publish_on_google_plus post
+    url = "https://plus.google.com/share?url=#{post_url(post)}"
+    link_to(image_tag(image_path("social_media/google.png")), url, target: "_blank")
+  end
+  def publish_on_twitter post
+    url = "http://twitter.com/share?text=#{post_url(post)}"
+    link_to(image_tag(image_path("social_media/twitter.png")), url, target: "_blank")
+  end
 end
