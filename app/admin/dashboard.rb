4f66e327090ddd83af592cef0674d394fa85fd5b
Paweł Placzyński <placek@ragnarson.com>
2013-07-14 17:26:42 +0200

    admin panel dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,22 @@
 ActiveAdmin.register_page "Dashboard" do
-
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span "Welcome to Active Admin. This is the default dashboard page."
-        small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
+  content title: "Pulpit" do
+    columns do
+      column do
+        panel "Cmentarz ewangelicko-augsburski" do
+          h5 "Ilość kwater: #{Part.find("lutheran").quarters.count}"
+          h5 "Ilość nagrobków: #{Part.find("lutheran").sepulchres.count}"
+        end
+        panel "Cmentarz katolicki" do
+          h5 "Ilość kwater: #{Part.find("catholic").quarters.count}"
+          h5 "Ilość nagrobków: #{Part.find("catholic").sepulchres.count}"
+        end
+        panel "Cmentarz prawosławny" do
+          h5 "Ilość kwater: #{Part.find("orthodox").quarters.count}"
+          h5 "Ilość nagrobków: #{Part.find("orthodox").sepulchres.count}"
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
