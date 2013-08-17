01efd6b7c5b78b5931e860d8829de577edc1517a
Paweł Placzyński <placek@ragnarson.com>
2013-08-17 14:38:39 +0200

    fixed tinymce problem

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -5,7 +5,6 @@
 $(function() {
   tinyMCE.init({
     mode : "textareas",
-    inline : true,
     menubar : false,
     toolbar_items_size : "small",
     toolbar : "undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | link image",
