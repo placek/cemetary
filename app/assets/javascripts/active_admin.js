abd66e0b194778660c55c7848586686d901bc475
Paweł Placzyński <placek@ragnarson.com>
2013-08-17 14:47:06 +0200

    tinymce language

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -7,6 +7,7 @@ $(function() {
     mode : "textareas",
     menubar : false,
     toolbar_items_size : "small",
+    language : "pl_PL",
     toolbar : "undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | link image",
     plugins : "link image",
   });
