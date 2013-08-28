f5b1e46d41096ea15823b560f54c123144e269db
Paweł Placzyński <placek@ragnarson.com>
2013-08-28 12:02:39 +0200

    kaminari and elastic search

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,7 +1,7 @@
 class PartsController < InheritedResources::Base
   def show
     @part = Part.find(params[:id])
-    @sepulchres = @part.sepulchres.search(params[:search]).per_page_kaminari(params[:page])
+    @sepulchres = @part.sepulchres.search(params[:search])
     respond_with(@part)
   end
 end
--- app/controllers/quarters_controller.rb
+++ app/controllers/quarters_controller.rb
@@ -2,7 +2,7 @@ class QuartersController < InheritedResources::Base
   def show
     @part = Part.find(params[:part_id])
     @quarter = @part.quarters.find(params[:id])
-    @sepulchres = @quarter.sepulchres.search(params[:search]).per_page_kaminari(params[:page])
+    @sepulchres = @quarter.sepulchres.search(params[:search])
     respond_with(@quarter)
   end
 end
