89b64d1f0147e212c70f87f49a3e9482b9444e48
Paweł Placzyński <placek@ragnarson.com>
2013-08-28 10:19:43 +0200

    simple elastic search for sepulchres; mappings

--- app/models/sepulchre.rb
+++ app/models/sepulchre.rb
@@ -13,6 +13,19 @@ class Sepulchre < ActiveRecord::Base
 
   paginates_per 15
 
+  mapping do
+    indexes :_id, index: :not_analyzed
+    indexes :name, analyzer: "snowball", boost: 100
+    indexes :surname, analyzer: "snowball", boost: 100
+    indexes :family_name, analyzer: "snowball", boost: 50
+    indexes :description, analyzer: "snowball"
+    indexes :birth_date, analyzer: "snowball"
+    indexes :burial_date, analyzer: "snowball"
+    indexes :birth_location, analyzer: "snowball"
+    indexes :gravestone, analyzer: "snowball"
+    indexes :complex, analyzer: "snowball"
+  end
+
   def self.search query
     tire.search load: true  do
       query { string query, default_operator: "AND" } if query.present?
